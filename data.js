fa76badc81df0fe64725251e2f2862f4592885b8
Chen Lin <clin@one-k.com>
2014-01-24 17:53:20 -0800

    add data

---
+++ data.js
@@ -0,0 +1,14 @@
+dataJson=[
+{
+	"title":"Pionner Stereo at night",
+	"url":"images/pioneerStereo.jpg",
+	"description":"Installed new stereo with matching color", 
+	"data":"2014/1/1"
+},
+{
+	"title":"60 Inch TV",
+	"url":"images/60inchTV.jpg",
+	"description":"No...Cannot take the 60 inch TV with remove it from the box.", 
+	"data":"2014/1/20"
+}
+];
